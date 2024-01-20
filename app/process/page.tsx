45b07e5753be614e33f0692e3692a7165b956041
ugur-sa <sadiklarugur@gmail.com>
2024-01-20 12:49:12 +0100

    Add AWS_URL to env

--- app/process/page.tsx
+++ app/process/page.tsx
@@ -12,19 +12,28 @@ export default async function ProcessPage({searchParams}: searchParams) {
 
     const url = searchParams.url;
 
-    const dockerURL = "http://flask-app:5000/process-url";
-    const localURL = "http://127.0.0.1:5000/process-url";
+    const AWS_URL = process.env.AWS_URL;
 
-    const response = await fetch(dockerURL, {
-        method: "POST",
-        body: JSON.stringify({ url: url }),
+    const awsURL = AWS_URL + "?url=" + url;
+
+    // const dockerURL = "http://flask-app:5000/process-url";
+    // const localURL = "http://127.0.0.1:5000/process-url";
+
+    const response = await fetch(awsURL, {
+        method: "GET",
         headers: {
           "Content-Type": "application/json",
         },
       });
       const data: ClassifiedText = await response.json();
-    
-      if(!data) { return <div>Loading...</div> }
+
+      if (response.status != 200) {
+        return (
+          <div className="flex justify-center items-center h-screen">
+            <p className="text-4xl font-bold text-red-500">Error</p>
+          </div>
+        );
+      }
 
     return (
         <SentimentPage classifiedText={data} />

a815e3d9b17c21b106f6343c0dc8a3415042f35d
ugur-sa <sadiklarugur@gmail.com>
2024-01-29 17:26:27 +0100

    Fix error

--- app/process/error.tsx
+++ app/process/error.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
+'use client';
+import React, { useEffect } from 'react';
 
-const Error: React.FC = () => {
+export default function Error({
+	error,
+}: {
+	error: Error & { digest?: string };
+}) {
+	useEffect(() => {
+		console.error(error);
+	}, [error]);
+
 	return (
-		<div>
+		<div className="flex h-dvh w-dvw flex-col items-center justify-center">
 			<h1>504 Gateway Timeout</h1>
 			<p>Sorry, the server is taking too long to respond.</p>
 			<p>Please refresh the page.</p>
+			<button
+				className="w-32 bg-black p-2 text-white"
+				onClick={() => window.location.reload()}
+			>
+				Refresh
+			</button>
 		</div>
 	);
-};
-
-export default Error;
+}

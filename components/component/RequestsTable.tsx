92571b34a2dd8c8e2fdb8edcb7ddf0f47091b4f4
ugur-sa <sadiklarugur@gmail.com>
2024-02-03 17:59:45 +0100

    Add tooltip for failed status to see error message

--- components/component/RequestsTable.tsx
+++ components/component/RequestsTable.tsx
@@ -3,6 +3,12 @@ import React from 'react';
 import FadeLoader from 'react-spinners/FadeLoader';
 import { SuccessIcon, FailedIcon } from '../ui/Icons';
 import { Requests } from '@/types/types';
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger,
+} from '../ui/tooltip';
 
 interface Props {
 	requests: Requests[];
@@ -12,7 +18,7 @@ const RequestsTable: React.FC<Props> = (props) => {
 	// Implement your component logic here
 	const requestLength = props.requests.length;
 	return (
-		<div className="no-scrollbar mb-4 w-fit overflow-scroll rounded-md border border-gray-300 bg-white px-8 pb-2 pt-8 shadow-xl">
+		<div className="mb-4 h-96 w-fit overflow-y-scroll rounded-md border border-gray-300 bg-white px-8 pb-2 pt-8 shadow-xl">
 			<h3 className="mb-4 pl-4 text-2xl font-semibold">Deine Requests</h3>
 			<table className="table-auto">
 				<thead>
@@ -33,6 +39,7 @@ const RequestsTable: React.FC<Props> = (props) => {
 								{request.status === 'completed' ? (
 									<Link
 										href={`/process?url=${request.url}`}
+										target="_blank"
 										className="text-blue-500"
 									>
 										{request.url}
@@ -42,26 +49,37 @@ const RequestsTable: React.FC<Props> = (props) => {
 								)}
 							</td>
 							<td className="">
-								{request.status === 'completed' ? (
-									<div className="flex w-full justify-center">
-										<SuccessIcon />
-									</div>
-								) : request.status === 'pending' ? (
-									<div className="sweet-loading ml-1 flex w-full justify-center">
-										<FadeLoader
-											color={'#0079d5'}
-											aria-label="Loading Spinner"
-											data-testid="loader"
-											height={15}
-											width={5}
-											margin={-1}
-										/>
-									</div>
-								) : (
-									<div className="flex w-full justify-center">
-										<FailedIcon />
-									</div>
-								)}
+								<div className="flex w-full justify-center">
+									<TooltipProvider>
+										<Tooltip>
+											<TooltipTrigger>
+												{request.status === 'completed' ? (
+													<SuccessIcon />
+												) : request.status === 'pending' ? (
+													<div className="sweet-loading ml-1">
+														<FadeLoader
+															color={'#0079d5'}
+															aria-label="Loading Spinner"
+															data-testid="loader"
+															height={15}
+															width={5}
+															margin={-1}
+														/>
+													</div>
+												) : (
+													<FailedIcon />
+												)}
+											</TooltipTrigger>
+											<TooltipContent>
+												{request.text !== undefined ? (
+													<div className="text-center">{request.text}</div>
+												) : (
+													<div className="text-center">{request.status}</div>
+												)}
+											</TooltipContent>
+										</Tooltip>
+									</TooltipProvider>
+								</div>
 							</td>
 						</tr>
 					))}

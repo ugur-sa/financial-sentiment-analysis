b164d1b2f51d55a35a55bf1c110f2b551bd2ff8f
ugur-sa <sadiklarugur@gmail.com>
2024-02-09 14:07:46 +0100

    Change to shadcnui card

--- components/component/sentiment-page.tsx
+++ components/component/sentiment-page.tsx
@@ -1,51 +1,126 @@
 import { ClassifiedText } from '@/types/types';
 import SentenceCard from '@/app/process/SentenceCard';
-import Card from './Card';
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card';
+
 import Link from 'next/link';
 interface SentimentPageProps {
 	classifiedText: ClassifiedText;
 }
 
 export function SentimentPage({ classifiedText }: SentimentPageProps) {
+	const overall_sentiment_text = classifiedText['Overall Sentiment'];
+
+	const textColor = () => {
+		if (typeof overall_sentiment_text === 'string') {
+			if (overall_sentiment_text === 'Bullish') {
+				return 'text-green-500';
+			} else if (overall_sentiment_text === 'Slightly Bullish') {
+				return 'text-green-300';
+			} else if (overall_sentiment_text === 'Neutral') {
+				return 'text-gray-500';
+			} else if (overall_sentiment_text === 'Slightly Bearish') {
+				return 'text-red-300';
+			}
+			return 'text-red-500';
+		}
+	};
+
 	return (
 		<>
-			<div className="flex h-full w-full flex-col-reverse xl:flex-row">
+			<div className="flex h-dvh w-dvw flex-col-reverse xl:flex-row">
 				<div className="grid h-full w-full grid-cols-2 place-items-center gap-2 p-12 xl:w-2/5">
-					<Card
-						title="Overall Sentiment"
-						value={classifiedText['Overall Sentiment']}
-					/>
-					<Card
-						title="Weighted Sentiment"
-						value={classifiedText['Weighted Sentiment']}
-					/>
-					<Card
-						title="Average Bullish Score"
-						value={classifiedText['Average Score Bullish']}
-					/>
-					<Card
-						title="Average Neutral Score"
-						value={classifiedText['Average Score Neutral']}
-					/>
-					<Card
-						title="Average Bearish Score"
-						value={classifiedText['Average Score Bearish']}
-					/>
-					<Card
-						title="Bullish Sentences"
-						value={classifiedText['Amount Bullish']}
-					/>
-					<Card
-						title="Neutral Sentences"
-						value={classifiedText['Amount Neutral']}
-					/>
-					<Card
-						title="Bearish Sentences"
-						value={classifiedText['Amount Bearish']}
-					/>
+					<Card className="h-48 w-full lg:w-11/12">
+						<CardHeader>
+							<CardTitle className="text-center text-lg lg:text-2xl">
+								Overall Sentiment
+							</CardTitle>
+						</CardHeader>
+						<CardContent
+							className={`${textColor()} text-center text-2xl font-bold lg:text-4xl`}
+						>
+							{classifiedText['Overall Sentiment']}
+						</CardContent>
+					</Card>
+					<Card className="h-48 w-full lg:w-11/12">
+						<CardHeader>
+							<CardTitle className="text-center text-lg lg:text-2xl">
+								Weighted Sentiment
+							</CardTitle>
+						</CardHeader>
+						<CardContent className="text-center text-xl font-bold lg:text-4xl">
+							{classifiedText['Weighted Sentiment'].toFixed(3)}
+						</CardContent>
+					</Card>
+					<Card className="h-48 w-full lg:w-11/12">
+						<CardHeader>
+							<CardTitle className="text-center text-lg lg:text-2xl">
+								Average Bullish Score
+							</CardTitle>
+						</CardHeader>
+						<CardContent className="text-center text-xl font-bold lg:text-4xl">
+							{classifiedText['Average Score Bullish'].toFixed(3)}
+						</CardContent>
+					</Card>
+					<Card className="h-48 w-full lg:w-11/12">
+						<CardHeader>
+							<CardTitle className="text-center text-lg lg:text-2xl">
+								Average Neutral Score
+							</CardTitle>
+						</CardHeader>
+						<CardContent className="text-center text-xl font-bold lg:text-4xl">
+							{classifiedText['Average Score Neutral'].toFixed(3)}
+						</CardContent>
+					</Card>
+					<Card className="h-48 w-full lg:w-11/12">
+						<CardHeader>
+							<CardTitle className="text-center text-lg lg:text-2xl">
+								Average Bearish Score
+							</CardTitle>
+						</CardHeader>
+						<CardContent className="text-center text-xl font-bold lg:text-4xl">
+							{classifiedText['Average Score Bearish'].toFixed(3)}
+						</CardContent>
+					</Card>
+					<Card className="h-48 w-full lg:w-11/12">
+						<CardHeader>
+							<CardTitle className="text-center text-lg lg:text-2xl">
+								Bullish Sentences
+							</CardTitle>
+						</CardHeader>
+						<CardContent className="text-center text-xl font-bold lg:text-4xl">
+							{classifiedText['Amount Bullish']}
+						</CardContent>
+					</Card>
+					<Card className="h-48 w-full lg:w-11/12">
+						<CardHeader>
+							<CardTitle className="text-center text-lg lg:text-2xl">
+								Neutral Sentences
+							</CardTitle>
+						</CardHeader>
+						<CardContent className="text-center text-xl font-bold lg:text-4xl">
+							{classifiedText['Amount Neutral']}
+						</CardContent>
+					</Card>
+					<Card className="h-48 w-full lg:w-11/12">
+						<CardHeader>
+							<CardTitle className="text-center text-lg lg:text-2xl">
+								Bearish Sentences
+							</CardTitle>
+						</CardHeader>
+						<CardContent className="text-center text-xl font-bold lg:text-4xl">
+							{classifiedText['Amount Bearish']}
+						</CardContent>
+					</Card>
 				</div>
 				{/*-------------------------------------------------------------------*/}
-				<div className="no-scrollbar flex h-full w-full flex-col items-center gap-2 overflow-scroll border border-l-2 p-3 xl:w-3/5">
+				<div className="no-scrollbar flex h-full w-full flex-col items-center gap-2 overflow-scroll border border-black p-3 xl:w-3/5">
 					{classifiedText.sentences.map((sentence: any, i: number) => (
 						<div
 							key={i}
